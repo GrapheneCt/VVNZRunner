45580c899c0c47de568f47f5b9b33c1a323fb189
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2021-08-24 02:39:58 +0200

    Update mep code

--- sample_mep/src/main.c
+++ sample_mep/src/main.c
@@ -1,6 +1,7 @@
 #define VENEZIA_SPRAM_ADDR	0xF1840000
 #define SPRAM_MEMSIZE		0x2500000
 #define SPRAM_SAFE_OFFSET	0x1400
+#define SPRAM_USE_BASE		(VENEZIA_SPRAM_ADDR + SPRAM_SAFE_OFFSET)
 
 void *vnzMemcpyToSpram(void *src, unsigned int size, unsigned int spramOffset);
 void *vnzMemcpyFromSpram(void *dst, unsigned int size, unsigned int spramOffset);
@@ -8,8 +9,8 @@ void *memcpy(void *s1, const void *s2, unsigned int n);
 
 void _start()
 {
-	unsigned int a1 = 0;
-	unsigned int a2 = 0x123;
+	int a1 = *(int *)(SPRAM_USE_BASE);
+	int a2 = *(int *)(SPRAM_USE_BASE + 4);
 
 	writeAnswer(a1 + a2);
 
@@ -17,9 +18,10 @@ void _start()
 		"jmp 0x80063C\n");
 }
 
-void writeAnswer(unsigned int res)
+void writeAnswer(int res)
 {
-	vnzMemcpyToSpram(&res, 4, SPRAM_SAFE_OFFSET);
+	*(int *)(SPRAM_USE_BASE + 8) = res;
+	//vnzMemcpyToSpram(&res, 4, SPRAM_SAFE_OFFSET);
 }
 
 void *vnzMemcpyToSpram(void *src, unsigned int size, unsigned int spramOffset)
