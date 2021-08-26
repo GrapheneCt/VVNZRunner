09a9bee98fbee7308bf1ccc7cf83ab20dd29f004
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2021-08-27 00:43:01 +0200

    Fix resume issue, make it calc powf

--- VVNZRunner/main.c
+++ VVNZRunner/main.c
@@ -3,6 +3,7 @@
 #include <scetypes.h>
 #include <kernel.h>
 #include <kernel/sysmem.h>
+#include <kernel/proc_event.h>
 #include <taihen.h>
 
 //#define TEST_BUILD
@@ -22,7 +23,8 @@ int(*sceVeneziaEventHandler)(int resume, int eventid, void *args, void *opt);
 #define IMAGE_MEMSIZE			0x400000
 #define SPRAM_MEMSIZE			0x2500000
 
-#define INJECT_CODE_SIZE_LIMIT	0x624
+#define INJECT_CODE_SIZE_LIMIT	0x1000
+#define INJECT_CODE_HALT_OFFSET	0x371A
 
 typedef struct SceCodecEngineWrapperThunkArg {
 	int processingResource;
@@ -38,7 +40,7 @@ typedef struct SceSysmemVeneziaImageParam {
 } SceSysmemVeneziaImageParam;
 
 static unsigned char s_injectCodeJumpDefault[4] = {
-	0x98, 0xDA, 0x37, 0x80
+	0x88, 0xDC, 0x37, 0x8E
 };
 
 static unsigned char s_origCodeJump[4];
@@ -49,10 +51,88 @@ static unsigned char s_injectCodeJump[4];
 static unsigned int s_requestedCodeOffset = 0;
 static unsigned int s_requestedCodeSize = 0;
 static SceUID s_vnzCodeRunSema = SCE_UID_INVALID_UID;
-static int s_keepAlive = 1;
+static SceUID s_vnzCodeCompleteSema = SCE_UID_INVALID_UID;
+static SceUID s_registeredPid = SCE_UID_INVALID_UID;
+static unsigned char s_keepAlive = 1;
+static unsigned char s_isAsync = 0;
+
+void _vnzDoRestore()
+{
+	memcpy(VADDR_VENEZIA_IMAGE + INJECT_CODE_HALT_OFFSET, s_origCodeJump, 4);
+	memcpy(VADDR_VENEZIA_IMAGE + s_requestedCodeOffset, s_origCode, s_requestedCodeSize);
+}
+
+void _vnzDoInject()
+{
+	memcpy(VADDR_VENEZIA_IMAGE + INJECT_CODE_HALT_OFFSET, s_injectCodeJump, 4);
+	memcpy(VADDR_VENEZIA_IMAGE + s_requestedCodeOffset, s_injectCode, s_requestedCodeSize);
+}
+
+void *vnzBridgeMapSpram()
+{
+	SceUID pid = sceKernelGetProcessId();
+
+	SceKernelAddrPair output[0x20];
+
+	SceKernelPaddrList PA_list;
+
+	PA_list.size = sizeof(PA_list);
+	PA_list.list_size = 0x20;
+	PA_list.ret_length = 1;
+	PA_list.ret_count = 1;
+	PA_list.list = output;
+
+	output[0].addr = 0x1F840000;
+	output[0].length = 0x1000;
+
+	// set fake paddr for test
+	for (int i = 1; i < 0x20; i++) {
+		output[i].addr = output[0].addr;
+		output[i].length = output[0].length;
+	}
+
+	PA_list.ret_length = 0x20;
+	PA_list.ret_count = 0x20;
+
+
+	// Start mapping to userland
+	SceKernelAllocMemBlockKernelOpt opt;
+	memset(&opt, 0, sizeof(opt));
+
+	opt.size = sizeof(opt);
+	opt.attr = SCE_KERNEL_ALLOC_MEMBLOCK_ATTR_HAS_PID | SCE_KERNEL_ALLOC_MEMBLOCK_ATTR_HAS_PADDR_LIST;
+	opt.pid = pid;
+	opt.paddr_list = &PA_list;
+
+	SceUID memid = sceKernelAllocMemBlock("SceUserMemoryMap", SCE_KERNEL_MEMBLOCK_TYPE_USER_RW, 0x20000, &opt);
+
+	void *user_base;
+	sceKernelGetMemBlockBase(memid, &user_base);
+
+	sceKernelCreateUserUid(pid, memid);
+
+	sceDebugPrintf("user_base : 0x%X\n", user_base);
+
+	return user_base;
+}
+
+int vnzBridgeRestore()
+{
+	if (s_registeredPid == SCE_UID_INVALID_UID)
+		return -4;
+
+	_vnzDoRestore();
+
+	s_registeredPid = SCE_UID_INVALID_UID;
+
+	return vnzBridgeExec(0);
+}
 
 int vnzBridgeInject(void *mepCodeJump, unsigned int mepCodeOffset, void *mepCode, unsigned int mepCodeSize)
 {
+	if (s_registeredPid != SCE_UID_INVALID_UID)
+		vnzBridgeRestore();
+
 	if (mepCodeSize > INJECT_CODE_SIZE_LIMIT)
 		return -1;
 
@@ -68,7 +148,7 @@ int vnzBridgeInject(void *mepCodeJump, unsigned int mepCodeOffset, void *mepCode
 		sceKernelMemcpyUserToKernel(s_injectCodeJump, mepCodeJump, 4);
 
 	if (!mepCodeOffset)
-		s_requestedCodeOffset = 0x3752; // as in s_injectCodeJumpDefault
+		s_requestedCodeOffset = 0xE3790; // as in s_injectCodeJumpDefault
 	else
 		s_requestedCodeOffset = mepCodeOffset;
 
@@ -77,12 +157,23 @@ int vnzBridgeInject(void *mepCodeJump, unsigned int mepCodeOffset, void *mepCode
 	memcpy(s_origCode, VADDR_VENEZIA_IMAGE + s_requestedCodeOffset, s_requestedCodeSize);
 	sceKernelMemcpyUserToKernel(s_injectCode, mepCode, s_requestedCodeSize);
 
+	s_registeredPid = sceKernelGetProcessId();
+
+	_vnzDoInject();
+
 	return SCE_OK;
 }
 
-int vnzBridgeExec()
+int vnzBridgeExec(int isAsync)
 {
-	return sceKernelSignalSema(s_vnzCodeRunSema, 1);
+	s_isAsync = isAsync;
+
+	sceKernelSignalSema(s_vnzCodeRunSema, 1);
+
+	if (!s_isAsync)
+		return sceKernelWaitSema(s_vnzCodeCompleteSema, 1, SCE_NULL);
+
+	return SCE_OK;
 }
 
 int vnzBridgeMemcpyToSpram(void *src, unsigned int size, unsigned int spramOffset)
@@ -106,7 +197,7 @@ int vnzBridgeGetSpramValue(unsigned int offset)
 	if (offset > SPRAM_MEMSIZE)
 		return -3;
 
-	unsigned int value = *(unsigned int *)(VADDR_VENEZIA_SPRAM + offset);
+	int value = *(unsigned int *)(VADDR_VENEZIA_SPRAM + offset);
 
 	return value;
 }
@@ -115,25 +206,54 @@ int vnzRunnerThread(SceSize args, void *argp)
 {
 	while (s_keepAlive) {
 		sceKernelWaitSema(s_vnzCodeRunSema, 1, SCE_NULL);
+
+#ifdef TEST_BUILD
 		sceDebugPrintf("begin\n");
+#endif
+
 		sceVeneziaEventHandler(0, 0x400e, NULL, NULL);
 
-		memcpy(VADDR_VENEZIA_IMAGE, s_injectCodeJump, 4);
-		memcpy(VADDR_VENEZIA_IMAGE + s_requestedCodeOffset, s_injectCode, s_requestedCodeSize);
+#ifdef TEST_BUILD
 		sceDebugPrintf("mid\n");
+#endif
+
 		sceVeneziaEventHandler(1, 0x1000e, NULL, NULL);
 
-		memcpy(VADDR_VENEZIA_IMAGE, s_origCodeJump, 4);
-		memcpy(VADDR_VENEZIA_IMAGE + s_requestedCodeOffset, s_origCode, s_requestedCodeSize);
+		if (!s_isAsync)
+			sceKernelSignalSema(s_vnzCodeCompleteSema, 1);
+
+#ifdef TEST_BUILD
 		sceDebugPrintf("end\n");
+#endif
 	}
 
 	return sceKernelExitDeleteThread(0);
 }
 
+int procEvHandler(SceUID pid, int event_type, SceProcEventInvokeParam1 *a3, int a4)
+{
+	if (event_type == 0x1000 && s_registeredPid == pid) {
+		_vnzDoRestore();
+		vnzBridgeExec(1);
+	}
+	else if (event_type == 0x40000 && s_registeredPid == pid) {
+		_vnzDoInject();
+
+	}
+
+	return SCE_OK;
+}
+
+int procEvExitHandler(SceUID pid, SceProcEventInvokeParam1 *a2, int a3)
+{
+	vnzBridgeRestore();
+
+	return SCE_OK;
+}
+
 int module_start(SceSize args, const void * argp)
 {
-	tai_module_info_t info;
+	static tai_module_info_t info;
 	info.size = sizeof(tai_module_info_t);
 	if (taiGetModuleInfoForKernel(KERNEL_PID, "SceCodecEngineWrapper", &info) < 0)
 		return SCE_KERNEL_START_SUCCESS;
@@ -144,12 +264,26 @@ int module_start(SceSize args, const void * argp)
 	if (s_vnzCodeRunSema <= 0)
 		return SCE_KERNEL_START_SUCCESS;
 
+	s_vnzCodeCompleteSema = sceKernelCreateSema("SceVeneziaCodeCompleteSema", 0, 0, 1, SCE_NULL);
+	if (s_vnzCodeCompleteSema <= 0)
+		return SCE_KERNEL_START_SUCCESS;
+
 	SceUID thid = sceKernelCreateThread("SceVeneziaRunnerThread", vnzRunnerThread, 0x10000100, 0x1000, 0, 0, NULL);
 	if (thid <= 0)
 		return SCE_KERNEL_START_SUCCESS;
 	sceKernelStartThread(thid, 0, NULL);
 
-	memcpy(s_origCodeJump, VADDR_VENEZIA_IMAGE, 4);
+	SceProcEventHandler handler;
+	memset(&handler, 0, sizeof(SceProcEventHandler));
+	handler.size = sizeof(SceProcEventHandler);
+	handler.stop = procEvHandler;
+	handler.start = procEvHandler;
+	handler.exit = procEvExitHandler;
+	handler.kill = procEvExitHandler;
+
+	sceKernelRegisterProcEventHandler("SceVeneziaProcEvHandler", &handler, 0);
+
+	memcpy(s_origCodeJump, VADDR_VENEZIA_IMAGE + INJECT_CODE_HALT_OFFSET, 4);
 
 	return SCE_KERNEL_START_SUCCESS;
 }
